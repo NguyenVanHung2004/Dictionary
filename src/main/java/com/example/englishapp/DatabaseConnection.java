1a7d8582a23233d7eb85fd6855d0345f63b30fea
NguyenVanHung2004 <125149885+NguyenVanHung2004@users.noreply.github.com>
2023-09-20 14:35:47 +0700

    Version 1.0 : Tra cứu bằng sql

---
+++ src/main/java/com/example/englishapp/DatabaseConnection.java
@@ -0,0 +1,24 @@
+package com.example.englishapp;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+
+public class DatabaseConnection {
+
+  public Connection databaseLink;
+
+  public Connection getDatabaseConnection() {
+    String databaseName = "entries";
+    String databaseUser = "root";
+    String databasePassWord = "Nguyenvanhung2004yb!";
+    String url = "jdbc:mysql://localhost/" + databaseName;
+    try {
+      Class.forName("com.mysql.cj.jdbc.Driver");
+      databaseLink = DriverManager.getConnection(url, databaseUser, databasePassWord);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return databaseLink;
+  }
+}
---
+++ src/main/java/com/example/englishapp/DictionaryApplicationController.java
@@ -0,0 +1,35 @@
+package com.example.englishapp;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class DictionaryApplicationController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+    @FXML
+    protected void goToSearchScreen(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("SearchScreen.fxml")));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root, 600 , 400);
+        stage.setScene(scene);
+
+    }
+}
