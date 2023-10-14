210b22a7b71e61aa4a566e7a36c49ae9825f8dcd
NguyenVanHung2004 <125149885+NguyenVanHung2004@users.noreply.github.com>
2023-10-14 23:16:03 +0700

    Version 1.5 : Update đa luồng ( thread)

--- java/module-info.java
+++ java/module-info.java
@@ -7,11 +7,17 @@
    requires org.json;
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
+     requires jlfgr;
+     requires freetts.jsapi10;
+     requires freetts;
+
     requires org.kordamp.bootstrapfx.core;
     requires com.almasb.fxgl.all;
     requires java.sql;
     requires mysql.connector.j;
      requires json.simple;
+     requires gtranslateapi;
+     requires jsapi;
      opens com.example.englishapp to javafx.fxml;
     exports com.example.englishapp;
 }
